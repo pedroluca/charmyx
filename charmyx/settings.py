817c8e885e082d8c1fd82b784f6783309ca976ec
Pietra Geovanna M. Ferreira <pietrag89@gmail.com>
2024-08-31 18:27:27 -0300

    feat: inserção do template do projeto

--- charmyx/settings.py
+++ charmyx/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'charmyx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR) + '/charmyx/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    str(BASE_DIR) + '/charmyx/static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
